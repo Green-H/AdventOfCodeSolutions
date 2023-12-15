c95d1f78a78dd35fe5891d2579371578d4846b3c
Green-H <vertolli.luca02@gmail.com>
2023-12-15 21:00:19 +0100

    Update sumEngineParts.py

--- day3/sumEngineParts.py
+++ day3/sumEngineParts.py
@@ -1,9 +1,9 @@
 # 141 caratteri per riga
-PATH = "day3/test.txt"
+PATH = "day3/schematics.txt"
 SYMBOLS = "=&+-@*%#/$"
 NUMBERS = "0123456789"
-MAXROW = 10
-MAXCOLUMN = 10
+MAXROW = 140
+MAXCOLUMN = 140
 def sumEngineParts():
     with open(PATH,"r") as file:
         num = ""
@@ -12,6 +12,7 @@ def sumEngineParts():
         data = file.readlines()
         for i in range(MAXROW):
             for j in range(MAXCOLUMN):
+                print("l'elemento in posizione",i,j)
                 current_element = data[i][j]
                 print("leggo: ",current_element)
                 if current_element in NUMBERS: # è un numero? se si mettilo in num
@@ -20,25 +21,35 @@ def sumEngineParts():
                 elif number_found and current_element not in NUMBERS: # non è un numero ma ne hai appena finito uno? 
                     print("Ho letto",current_element,"e l'ultimo numero e",num)
                     number_found = False
+                    # Awkward checks ?
                     if i == 0:
                         top_row = ""
                     else:
+                        print("creo la riga sotto di me, sono alla riga i=",i)
                         top_row = data[i-1][max(0,j-len(num)-1):j+1]
                     if i == MAXROW-1:
                         bot_row=""
                     else:
                         bot_row = data[i+1][max(0,j-len(num)-1):j+1]
-
-                    print("[i,j]=",i,j,"devo controllare :",top_row,"e",bot_row)
+                    if j == MAXCOLUMN-1 and data[i][j] in NUMBERS:
+                        right_side = ""
+                    else:
+                        right_side = data[i][j]
+                    if j == 0:
+                        left_side = ""
+                    else:
+                        left_side = data[i][j-len(num)-1]
+                    print("[i,j]=",i,j,"\n","Check left: ",left_side,"Check right: ",right_side,"Check up: ",top_row,"Check down: ",bot_row)
                     # se è adiacete ad un simbolo il numero è valido e lo aggiungo al totale, resettandolo
-                    if data[i][j] in SYMBOLS or data[i][j-len(num)-1] in SYMBOLS or any(char in top_row for char in SYMBOLS) or any(char in bot_row for char in SYMBOLS):
-                        #tot += int(num)
+                    if right_side in SYMBOLS or left_side in SYMBOLS or any(char in top_row for char in SYMBOLS) or any(char in bot_row for char in SYMBOLS):
+                        tot += int(num)
                         print("numero rilevato dopo  un simbolo:",num)
                     else:
                         print("numero non valido")
                     num = ""
-        print("===========")            
+        print("===================")            
         print("il totale e",tot)
+        print("===================")
         
 
 sumEngineParts()
