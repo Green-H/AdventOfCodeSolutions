82964d87de7551a7750af363099969636cf11752
Green-H <vertolli.luca02@gmail.com>
2023-12-16 18:03:13 +0100

    Create calcGearRatio.py

---
+++ day3/calcGearRatio.py
@@ -0,0 +1,16 @@
+PATH = "input/day3/schematics.txt"
+NUMBERS = "0123456789"
+MAXROW = 140
+MAXCOLUMN = 140
+
+def calcGearRatio():
+    with open(PATH,"r")as file:
+        data = file.readlines()
+        for i in range(MAXROW):
+            for j in range(MAXCOLUMN-1):
+                current_element = data[i][j]
+                if current_element == "*":
+                     
+
+
+calcGearRatio()
